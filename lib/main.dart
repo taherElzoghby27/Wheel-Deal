3edc2dae90b85361a4cfd10bc428ab6d8d89b4a2
taherelzoghby <taheramin442@gmail.com>
2023-12-26 22:14:52 +0200

    added fonts to my app

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: 'Raleway',
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
