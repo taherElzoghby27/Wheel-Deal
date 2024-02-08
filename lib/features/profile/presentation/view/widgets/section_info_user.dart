44db9cc88649d99b27cb8c1fc3b4c3f3a5b6b99c
taherelzoghby <taheramin442@gmail.com>
2024-02-08 21:47:05 +0200

    edited size width for user name

--- lib/features/profile/presentation/view/widgets/section_info_user.dart
+++ lib/features/profile/presentation/view/widgets/section_info_user.dart
@@ -1,12 +1,14 @@
 import 'package:cars/core/consts/style.dart';
 import 'package:cars/core/widgets/custom_circle_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SectionInfoUser extends StatelessWidget {
   const SectionInfoUser({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
     return Column(
       children: [
         const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
@@ -16,10 +18,15 @@ class SectionInfoUser extends StatelessWidget {
           onTap: () {},
         ),
         const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
-        Text(
-          'Taher Elzoghby',
-          style: AppConsts.style20.copyWith(
-            color: AppConsts.neutral900,
+        SizedBox(
+          width: size.width*.5.w,
+          child: Text(
+            'Rock',
+            style: AppConsts.style20.copyWith(
+              color: AppConsts.neutral900,
+            ),
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
           ),
         ),
       ],
