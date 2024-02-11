996c40a3a77b6f5a42d0f5e4cec8571be8269692
taherelzoghby <taheramin442@gmail.com>
2024-02-11 19:55:13 +0200

    completed edit profile page

--- lib/features/edit_profile/presentation/view/widgets/edit_profile_body.dart
+++ lib/features/edit_profile/presentation/view/widgets/edit_profile_body.dart
@@ -1,12 +1,48 @@
+import 'package:cars/core/consts/strings.dart';
+import 'package:cars/core/consts/style.dart';
+import 'package:cars/core/widgets/custom_app_bar.dart';
+import 'package:cars/core/widgets/custom_button.dart';
+import 'package:cars/core/widgets/custom_divider.dart';
+import 'package:cars/features/edit_profile/presentation/view/widgets/fields_section.dart';
+import 'package:cars/features/profile/presentation/view/widgets/section_info_user.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class EditProfileBody extends StatelessWidget {
   const EditProfileBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return ListView(
+      children: [
+        const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
+        //   title
+        CustomAppBar(
+          leadingOnTap: ()=>GoRouter.of(context).pop(),
+          title: StringsEn.editProfile,
+          trailingOnTap: () {},
+        ),
+        //User info
+        const SectionInfoUser(),
+        const CustomDivider(),
+
+        //fields
+        const FieldsSection(),
+        const AspectRatio(aspectRatio: AppConsts.aspectRatioButtonDetailsCar),
+        //done button
+
+        Padding(
+          padding:  EdgeInsets.symmetric(horizontal: 10.w),
+          child: AspectRatio(
+            aspectRatio: AppConsts.aspectRatioButtonAuth.sp,
+            child: CustomButton(
+              text: StringsEn.done,
+              onTap: () {},
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/edit_profile/presentation/view/widgets/fields_section.dart
@@ -0,0 +1,44 @@
+import 'package:cars/core/consts/strings.dart';
+import 'package:cars/core/consts/style.dart';
+import 'package:cars/core/widgets/text_form_field.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FieldsSection extends StatelessWidget {
+  const FieldsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 20.w),
+      child: Column(
+        children: [
+          const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
+          CustomTextFormField(
+            hint: StringsEn.firstName,
+          ),
+          const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
+          CustomTextFormField(
+            hint: StringsEn.lastName,
+          ),
+          const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
+          CustomTextFormField(
+            hint: StringsEn.phoneNumber,
+            perfixIcon: CountryCodePicker(
+              initialSelection: StringsEn.eg,
+              flagWidth: 25,
+              onChanged: (CountryCode code) {},
+              showDropDownButton: true,
+              showCountryOnly: true,
+              showOnlyCountryWhenClosed: true,
+              showFlagDialog: true,
+              hideMainText: true,
+              showFlagMain: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
