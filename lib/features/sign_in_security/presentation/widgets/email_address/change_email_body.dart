e320f393881330c7736ff68c8b0663238514baea
taherelzoghby <taheramin442@gmail.com>
2024-04-24 06:34:37 +0200

    added change email page

--- lib/features/sign_in_security/presentation/widgets/email_address/change_email_body.dart
+++ lib/features/sign_in_security/presentation/widgets/email_address/change_email_body.dart
@@ -8,33 +8,55 @@ class ChangeEmailBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        AspectRatio(aspectRatio: AppConsts.aspectRatio40on1),
-        CustomTextFormField(
-          hint: StringsEn.newEmailAddress,
-          perfixIcon: Icon(
-            Icons.email,
-            color: AppConsts.neutral500,
+    return Padding(
+      padding: AppConsts.mainPadding,
+      child: Column(
+        children: [
+          const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
+          const CustomTextFormField(
+            hint: StringsEn.newEmailAddress,
+            perfixIcon: Icon(
+              Icons.email,
+              color: AppConsts.neutral500,
+            ),
           ),
-        ),
-        AspectRatio(aspectRatio: AppConsts.aspectRatio40on1),
-        CustomTextFormField(
-          hint: StringsEn.confirmNewEmail,
-          perfixIcon: Icon(
-            Icons.email,
-            color: AppConsts.neutral500,
+          const AspectRatio(aspectRatio: AppConsts.aspectRatio20on1),
+          const CustomTextFormField(
+            hint: StringsEn.confirmNewEmail,
+            perfixIcon: Icon(
+              Icons.email,
+              color: AppConsts.neutral500,
+            ),
           ),
-        ),
-        AspectRatio(aspectRatio: AppConsts.aspectRatio40on1),
-        CustomTextFormField(
-          hint: StringsEn.yourPassword,
-          perfixIcon: Icon(
-            Icons.lock,
-            color: AppConsts.neutral500,
+          const AspectRatio(aspectRatio: AppConsts.aspectRatio20on1),
+          CustomTextFormField(
+            hint: StringsEn.yourPassword,
+            perfixIcon: const Icon(
+              Icons.lock,
+              color: AppConsts.neutral500,
+            ),
+            suffixIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.visibility,
+                color: AppConsts.neutral500,
+              ),
+            ),
           ),
-        ),
-      ],
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                StringsEn.forgotPass,
+                style: AppConsts.style12.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
