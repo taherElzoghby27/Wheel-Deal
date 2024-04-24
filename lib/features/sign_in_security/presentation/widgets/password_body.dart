9c52831e30b0d2ce5d534e1e1aa0a133f5aae42c
taherelzoghby <taheramin442@gmail.com>
2024-04-24 07:02:44 +0200

    added change password page

--- lib/features/sign_in_security/presentation/widgets/password_body.dart
+++ lib/features/sign_in_security/presentation/widgets/password_body.dart
@@ -1,12 +1,63 @@
+import 'package:cars/core/consts/strings.dart';
+import 'package:cars/core/consts/style.dart';
+import 'package:cars/core/widgets/customButton.dart';
+import 'package:cars/core/widgets/text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class PasswordBody extends StatelessWidget {
   const PasswordBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Padding(
+      padding: AppConsts.mainPadding,
+      child: Column(
+        children: [
+          const AspectRatio(aspectRatio: AppConsts.aspectRatioTopSpace),
+          const CustomTextFormField(
+            hint: StringsEn.yourPassword,
+            perfixIcon: Icon(
+              Icons.lock,
+              color: AppConsts.neutral500,
+            ),
+          ),
+          const AspectRatio(aspectRatio: AppConsts.aspectRatio20on1),
+           CustomTextFormField(
+            hint: StringsEn.enterNewPass,
+            perfixIcon: const Icon(
+              Icons.lock,
+              color: AppConsts.neutral500,
+            ),
+          ),
+          const AspectRatio(aspectRatio: AppConsts.aspectRatio20on1),
+          CustomTextFormField(
+            hint: StringsEn.confirmNewPass,
+            perfixIcon: const Icon(
+              Icons.lock,
+              color: AppConsts.neutral500,
+            ),
+            suffixIcon: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.visibility,
+                color: AppConsts.neutral500,
+              ),
+            ),
+          ),
+          const AspectRatio(aspectRatio: AppConsts.aspectRatio16on5),
+          AspectRatio(
+            aspectRatio: AppConsts.aspectRatioButtonAuth,
+            child: CustomButton(
+              text: StringsEn.confirmNewPass,
+              styleText: AppConsts.style16White.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+              onTap: () => GoRouter.of(context).push(''),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
