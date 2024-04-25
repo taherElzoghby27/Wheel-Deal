f01d4377b103a2f59a0f90b6a1f09eacf7e1fcf7
taherelzoghby <taheramin442@gmail.com>
2024-04-25 19:55:56 +0200

    added dialog to delete

--- lib/features/sign_in_security/presentation/widgets/two_step_method.dart
+++ lib/features/sign_in_security/presentation/widgets/two_step_method.dart
@@ -1,7 +1,11 @@
 import 'package:cars/core/consts/strings.dart';
 import 'package:cars/core/consts/style.dart';
+import 'package:cars/core/widgets/customButton.dart';
+import 'package:cars/core/widgets/custom_divider.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+import '../../../../core/widgets/custom_button_with_border.dart';
 import 'two_step_method_tile.dart';
 
 class TwoStepMethod extends StatelessWidget {
@@ -26,7 +30,67 @@ class TwoStepMethod extends StatelessWidget {
                   ),
                 ),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => Dialog(
+                        surfaceTintColor: Theme.of(context).primaryColor,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const AspectRatio(
+                              aspectRatio: AppConsts.aspectRatio24on2,
+                            ),
+                            const Center(
+                              child: Text(
+                                StringsEn.delete,
+                                style: AppConsts.style16White,
+                              ),
+                            ),
+                            Padding(
+                              padding: AppConsts.mainPadding,
+                              child: const CustomDivider(),
+                            ),
+                            const AspectRatio(
+                              aspectRatio: AppConsts.aspectRatio24on2,
+                            ),
+                            const Text(
+                              StringsEn.areYouSureYouWantToRemove,
+                              style: AppConsts.styleHint14,
+                            ),
+                            const AspectRatio(
+                              aspectRatio: AppConsts.aspectRatio24on2,
+                            ),
+                            Row(
+                              children: [
+                                const Spacer(flex: 1),
+                                Expanded(
+                                  flex: 5,
+                                  child: CustomButtonWithBorder(
+                                    text: StringsEn.cancel,
+                                    onTap: () => GoRouter.of(context).pop(),
+                                  ),
+                                ),
+                                const Spacer(flex: 1),
+                                Expanded(
+                                  flex: 5,
+                                  child: CustomButton(
+                                    text: StringsEn.remove,
+                                    styleText: AppConsts.style16White,
+                                    onTap: () {},
+                                  ),
+                                ),
+                                const Spacer(flex: 1),
+                              ],
+                            ),
+                            const AspectRatio(
+                              aspectRatio: AppConsts.aspectRatio40on1,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
                   icon: const Icon(
                     Icons.delete,
                     color: AppConsts.mainColor,
