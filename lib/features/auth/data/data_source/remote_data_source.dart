ae051a0afca8f99a1f9406bcee67479a25fa8a0f
taherelzoghby <taheramin442@gmail.com>
2024-05-08 22:31:20 +0300

    add remote data source

--- lib/features/auth/data/data_source/remote_data_source.dart
+++ lib/features/auth/data/data_source/remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:cars/core/consts/api.dart';
+import 'package:cars/core/services/api_service.dart';
 import 'package:cars/features/auth/data/models/user_model.dart';
 
 abstract class RemoteDataSource {
@@ -9,27 +11,41 @@ abstract class RemoteDataSource {
 
   Future<UserModel> verifyEmail({required UserModel userEntity});
 }
-class RemoteDataSourceImpl extends RemoteDataSource{
+
+class RemoteDataSourceImpl extends RemoteDataSource {
+  final ApiService _apiService;
+
+  RemoteDataSourceImpl({required ApiService apiService})
+      : _apiService = apiService;
+
   @override
-  Future<UserModel> login({required UserModel userEntity}) {
-    // TODO: implement login
+  Future<UserModel> login({required UserModel userEntity}) async {
+    Map<String, dynamic> result = await _apiService.post(
+      endPoint: ApiConsts.loginEndpoint,
+      data: userEntity.toMapLogin(),
+    );
+    UserModel userModel = UserModel.fromMap(result);
+    return userModel;
+  }
+
+  @override
+  Future<UserModel> signUp({required UserModel userEntity}) async {
+    Map<String, dynamic> result = await _apiService.post(
+      endPoint: ApiConsts.loginEndpoint,
+      data: userEntity.toMapSignUp(),
+    );
+    UserModel userModel = UserModel.fromMap(result);
+    return userModel;
+  }
+
+  @override
+  Future<UserModel> verifyEmail({required UserModel userEntity}) async {
+    // TODO: implement verifyEmail
     throw UnimplementedError();
   }
 
   @override
-  Future<UserModel> resetPassword({required UserModel userEntity}) {
-    // TODO: implement resetPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<UserModel> signUp({required UserModel userEntity}) {
-    // TODO: implement signUp
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<UserModel> verifyEmail({required UserModel userEntity}) {
+  Future<UserModel> resetPassword({required UserModel userEntity}) async {
     // TODO: implement verifyEmail
     throw UnimplementedError();
   }
