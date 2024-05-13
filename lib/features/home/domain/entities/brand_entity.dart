afd31a419f5d1497d5be78a547b7bed260deaaa3
taherelzoghby <taheramin442@gmail.com>
2024-05-13 21:42:13 +0300

    added entities for home

---
+++ lib/features/home/domain/entities/brand_entity.dart
@@ -0,0 +1,11 @@
+class BrandEntity {
+  final int id;
+  final String name;
+  final String logo;
+
+  BrandEntity({
+    required this.id,
+    required this.name,
+    required this.logo,
+  });
+}
---
+++ lib/features/home/domain/entities/car_entity.dart
@@ -0,0 +1,13 @@
+class CarEntity {
+  final int id;
+  final String image;
+  final String title;
+  final num price;
+
+  CarEntity({
+    required this.id,
+    required this.image,
+    required this.title,
+    required this.price,
+  });
+}
